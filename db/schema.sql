03de79af7bf7bec42fa5072d9d095c770b7bc2e8
Jose Castellanos <josecastellanos@Joses-MacBook-Pro-2.local>
2018-06-04 21:19:34 -0700

    some styling done to the page

---
+++ db/schema.sql
@@ -0,0 +1,21 @@
+-- Create a burgers table with these fields:
+
+-- id: an auto incrementing int that serves as the primary key.
+
+-- burger_name: a string.
+
+-- devoured: a boolean.
+
+CREATE DATABASE burgers_db;
+
+USE DATABASE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(55),
+devoured something goes here
+PRIMARY KEY(id)
+);
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name,devoured)
+ VALUES ("chesse burger");
+INSERT INTO burgers (burger_name,devoured)
+ VALUES ("Big Mac");
+INSERT INTO burgers (burger_name,devoured)
+ VALUES ("Mc Chicken");
